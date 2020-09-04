53afff8b0b006f5e31d4a85eb6d3d0c1218da54d
Yatharth arora <yathartharora1999@gmail.com>
2020-09-05 04:24:54 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -3,6 +3,9 @@ const routes = require('next-routes')();
 
 
 routes
+.add("/","/index")
+.add("/developer","/developer")
+.add("/about","/about")
 .add("/newmatch","/matches/newmatch")
 .add('/matches/:address', '/matches/show')
 module.exports = routes;
